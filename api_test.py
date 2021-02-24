647d5c6e72a4f7bdd2f59772b4f3fce13cb53e1b
twinsminsk <twinsminsk@yandex.ru>
2021-02-24 15:12:15 +0300

    Yclients class for API

--- api_test.py
+++ api_test.py
@@ -1,21 +1,21 @@
-import requests
+from yclients.api import YClients
 
 BEARER = None
 USER = None
 COMPANY_ID = None
-
 headers = {
   'Content-Type': 'application/json',
   'Authorization': f'Bearer {BEARER}, User {USER}',
   'Accept': 'application/vnd.yclients.v2+json',
 }
 
-response = requests.get(f'https://api.yclients.com/api/v1/company/{COMPANY_ID}/services/', headers=headers)
-for obj in response.json()["data"]:
+yclients = YClients(bearer=BEARER, user=USER, company_id=COMPANY_ID)
+
+for obj in yclients.get_services():
     print(f'{obj["title"]} ({obj["price_min"]}-{obj["price_max"]})')
 
     for staff_obj in obj['staff'][0]:
-        staff_response = requests.get(f'https://api.yclients.com/api/v1/company/{COMPANY_ID}/staff/{staff_obj["id"]}/', headers=headers).json()
-        print(staff_response["data"]["name"])
+        staff_response = yclients.get_staff_info(staff_obj["id"])
+        print(staff_response["name"])
 
     print("=" * 50)
